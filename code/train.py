56d5feaada949297f73b66a61f17d0a90444ffc5
piptouque <piptouque@protonmail.com>
2021-12-26 02:08:08 +0100

    [+]: better file organisation.

--- code/train.py
+++ code/train.py
@@ -6,9 +6,9 @@ import data_loader.data_loaders as module_data
 import model.loss as module_loss
 import model.metric as module_metric
 import model.model as module_arch
-from parse_config import ConfigParser
 from trainer import Trainer
 from utils import prepare_device
+from utils.parse_config import ConfigParser
 
 
 # fix random seeds for reproducibility
